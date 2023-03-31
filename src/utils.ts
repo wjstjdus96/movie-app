65519fb781b1fe98c50b49a1cfa60cea3308d4f3
seoyeon <wjstjdus96@naver.com>
2023-03-31 22:27:47 +0900

    fix: 이미지 import를 위한 처리

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,11 @@
+import noImage from "./assets/images/noImage.png";
+
 export function makeImagePath(id: string, format?: string) {
-  return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
+  if (id === "" || id === null || id === undefined) {
+    return noImage;
+  } else {
+    return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
+  }
+
+  // return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
 }

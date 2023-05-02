76d663c77e07b57df241373f0f8f24b37f270a72
seoyeon <wjstjdus96@naver.com>
2023-05-02 18:25:44 +0900

    fix: 모달 오버레이 화면 꽉차게 수정

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -15,6 +15,7 @@ import RelatedMovie from "./RelatedMovie";
 const Overlay = styled(motion.div)`
   position: fixed;
   top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   background-color: rgb(0, 0, 0, 0.5);
